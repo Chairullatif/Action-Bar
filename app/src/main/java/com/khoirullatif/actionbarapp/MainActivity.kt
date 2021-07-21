6cd512f26e0ce4e03f11bf762b86d20bb910c196
Chairullatif Aji Sadewa <khoirullatif337@gmail.com>
2021-07-21 19:19:55 +0700

    menambahkan serchview

--- app/src/main/java/com/khoirullatif/actionbarapp/MainActivity.kt
+++ app/src/main/java/com/khoirullatif/actionbarapp/MainActivity.kt
@@ -1,10 +1,14 @@
 package com.khoirullatif.actionbarapp
 
+import android.app.SearchManager
+import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.SearchView
+import android.widget.Toast
 import com.khoirullatif.actionbarapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -17,9 +21,29 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
     }
 
-    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
         val inflater = menuInflater
         inflater.inflate(R.menu.option_menu,menu)
+
+        //untuk menangani SEARCHVIEW
+        val searchManager = getSystemService(Context.SEARCH_SERVICE) as SearchManager
+        val searchView = menu.findItem(R.id.search).actionView as SearchView
+
+        searchView.setSearchableInfo(searchManager.getSearchableInfo(componentName))
+        searchView.queryHint = resources.getString(R.string.search_hint)
+        searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+            //ketika kata disubmit
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                Toast.makeText(this@MainActivity, query, Toast.LENGTH_SHORT).show()
+                return true
+            }
+
+            //ketika ada perubahan huruf
+            override fun onQueryTextChange(newText: String?): Boolean {
+                return false
+            }
+        })
+
         return true
     }
 
